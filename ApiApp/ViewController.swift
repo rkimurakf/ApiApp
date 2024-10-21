f29c4ccca8c89cb92b63d39f9a76c8bd4b10d263
ryuuto.kimura <r_kimura@kyoei-e.com>
2024-10-21 17:46:38 +0900

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by mba2408.silver kyoei.engine on 2024/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
